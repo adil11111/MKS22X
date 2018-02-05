f0eff93ffab4ccc097efb8e53461dd503b428c8b
Adil Gondal <agondal@stuy.edu>
2018-02-04 20:07:09 -0500

    made fact and fib functions

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,2 +1,35 @@
 public class Recursion{
+    public int fact(int n){
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}
+	if (n==1){
+	    return 1;
+	}
+	return n * fact(n-1);
+    }
+
+    public int fib (int n){
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}
+	if (n < 2){
+	    return n;
+	}
+	return Math.abs(fibHelp(1,n, 0,1));
+    }
+
+    public int fibHelp(int counter,int n,int previous,int current){
+	if (counter==n){
+	    return current;
+	}
+	return fibHelp(counter+1,n,current,previous+current);
+    }
+	
+    
+    /*public static void main(String[] args){
+	Recursion test = new Recursion();
+	System.out.println(test.fib(47));
+    }*/
 }
+
