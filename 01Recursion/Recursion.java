74f0893ce245031c6ba4991e30a0f97c8d43bfa2
Adil Gondal <agondal@stuy.edu>
2018-02-05 19:13:16 -0500

    added sqrt function

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -16,7 +16,7 @@ public class Recursion{
 	if (n < 2){
 	    return n;
 	}
-	return Math.abs(fibHelp(1,n, 0,1));
+	return fibHelp(1,n, 0,1);
     }
 
     public int fibHelp(int counter,int n,int previous,int current){
@@ -25,11 +25,29 @@ public class Recursion{
 	}
 	return fibHelp(counter+1,n,current,previous+current);
     }
+     public double sqrt(double n){
+	return sqrtHelp(n, n/2);
+    }
+    public double sqrtHelp(double n, double guess){
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}
+	if (n < 2){
+	    return n;
+	}
+	double boundary = n * 0.000001;
+	if (((guess * guess) < (n + boundary)) && ((guess * guess) > (n - boundary))){
+	    return guess;
+	}
+	return sqrtHelp(n,( n / guess + guess) / 2);
+	
+    }
 	
     
-    /*public static void main(String[] args){
+    /* public static void main(String[] args){
 	Recursion test = new Recursion();
-	System.out.println(test.fib(47));
-    }*/
+	System.out.println(test.sqrt(-1));
+	//System.out.println(100*0.0000001);
+	}*/
 }
 
