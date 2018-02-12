2ed23711ea1c75912c2a3d4e6261c16de6bcbb37
Adil Gondal <agondal@stuy.edu>
2018-02-12 14:40:59 -0500

    added remove/add queen methods

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -11,8 +11,32 @@ public class QueenBoard{
     }
     
     
-    //private boolean addQueen(int r, int c){}
-    //private boolean removeQueen(int r, int c){}
+    private boolean addQueen(int r, int c){
+	if (board[r][c]==0){
+	    board[r][c]=-1;
+	    for (int row= r;row<board.length-1;row++){
+		board[row+1][row+1]+=1;
+	    }
+	    for (int col=c+1;col<board[0].length;col++){
+		board[r][col]+=1;
+	    }
+	return true;
+	}
+	return false;
+    }
+    private boolean removeQueen(int r, int c){
+	if (board[r][c]==-1){
+	    board[r][c]=0;
+	    for (int row= r;row<board.length-1;row++){
+		board[row+1][row+1]-=1;
+	    }
+	    for (int col=c+1;col<board[0].length;col++){
+		board[r][col]-=1;
+	    }
+	return true;
+	}
+	return false;
+    }
 
     public String toString(){
 	String gatherer="";
@@ -29,6 +53,9 @@ public class QueenBoard{
 
     public static void main(String[] args){
         QueenBoard test = new QueenBoard(5);
+	test.addQueen(0,0);
+	System.out.println(test);
+	test.addQueen(2,1);
 	System.out.println(test);
     }
 }
