1d56ec4b381a660b1a4b85d72add6c55733b9b3d
Adil Gondal <agondal@stuy.edu>
2018-02-14 01:10:51 -0500

    more fixes but still not working properly

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -13,55 +13,68 @@ public class QueenBoard{
     
     private boolean addQueen(int r, int c){
 	try{
-	    int row=r;
-	    int col=c;
+	int row=r;
+	int col=c;
 	    if (board[r][c]==0){
 		board[r][c]=-1;
+		while (row> 0 && col < board.length-1){
+		    board[row-1][col+1]+=1;
+		    row--;
+		    col++;
+		}
 		row=r;
 		col=c+1;
 	        while (col <board[0].length){
-		    System.out.println("yay");
 		    board[r][col]+=1;
 		    col++;
 		}
 		row=r;
 		col=c;
-		while (row< board.length && col < board[0].length){
+		while (row< board.length-1 && col < board[0].length-1){
 		    board[row+1][col+1]+=1;
 		    row++;
 		    col++;
 		}
-		row=r;
-		col=c;
-		while (row> 0 && col > 0){
-		    board[row-1][col-1]+=1;
-		    row--;
-		    col--;
-		}
-		
+				
 	    }
 		return true;
 	}
-	catch(ArrayIndexOutOfBoundsException e){}
+	catch(ArrayIndexOutOfBoundsException e){
+	    e.printStackTrace(System.out);}
 	return false;
     }
-    private boolean removeQueen(int r, int c){
-	if (board[r][c]==-1){
-	    board[r][c]=0;
-	    for (int row= r;row<board.length-1;row++){
-		board[row+1][row+1]-=1;
+     private boolean removeQueen(int r, int c){
+	try{
+	int row=r;
+	int col=c;
+	    if (board[r][c]==-1){
+		board[r][c]=0;
+		while (row> 0 && col < board.length-1){
+		    board[row-1][col+1]-=1;
+		    row--;
+		    col++;
+		}
+		row=r;
+		col=c+1;
+	        while (col <board[0].length){
+		    board[r][col]-=1;
+		    col++;
+		}
+		row=r;
+		col=c;
+		while (row< board.length-1 && col < board[0].length-1){
+		    board[row+1][col+1]-=1;
+		    row++;
+		    col++;
+		}
+				
 	    }
-	    for (int col=c+1;col<board[0].length;col++){
-		board[r][col]-=1;
-	    }
-	    for (int row=r;row>0;row--){
-		board[row-1][row-1]-=1;
-	    }
-	return true;
+		return true;
 	}
+	catch(ArrayIndexOutOfBoundsException e){
+	    e.printStackTrace(System.out);}
 	return false;
     }
-
     public String toString(){
 	String gatherer="";
 	for(int r=0;r<board.length;r++){
@@ -86,13 +99,17 @@ public class QueenBoard{
 	}
 	return false;
     }
+
     //public int countSolution(){}
 
     public static void main(String[] args){
-        QueenBoard test = new QueenBoard(5);
+        QueenBoard test = new QueenBoard(2);
 	//test.addQueen(0,0);
 	//System.out.println(test);
-	test.addQueen(2,2);
+	/*test.addQueen(2,1);
+	System.out.println(test);
+	test.removeQueen(2,1);
+	System.out.println(test);*/
 	System.out.println(test);
     }
 }
