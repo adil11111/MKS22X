0a21703785b56acf315fd327ad1ff34ee7988058
Adil Gondal <agondal@stuy.edu>
2018-02-14 16:36:17 -0500

    fixed solve and count methods

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -10,106 +10,135 @@ public class QueenBoard{
 	}
     }
     
-    
+      
     private boolean addQueen(int r, int c){
-	try{
 	int row=r;
 	int col=c;
-	    if (board[r][c]==0){
-		board[r][c]=-1;
-		while (row> 0 && col < board.length-1){
-		    board[row-1][col+1]+=1;
-		    row--;
-		    col++;
-		}
-		row=r;
-		col=c+1;
-	        while (col <board[0].length){
-		    board[r][col]+=1;
-		    col++;
-		}
-		row=r;
-		col=c;
-		while (row< board.length-1 && col < board[0].length-1){
-		    board[row+1][col+1]+=1;
-		    row++;
-		    col++;
-		}
-				
+	if (board[r][c]==0){
+	    board[r][c]=-1;
+	    while (row> 0 && col < board.length-1){
+		board[row-1][col+1]+=1;
+		row--;
+		col++;
 	    }
-		return true;
+	    row=r;
+	    col=c+1;
+	    while (col <board[0].length){
+		board[r][col]+=1;
+		col++;
+	    }
+	    row=r;
+	    col=c;
+	    while (row< board.length-1 && col < board[0].length-1){
+		board[row+1][col+1]+=1;
+		row++;
+		col++;
+	    }
+	    return true;		
 	}
-	catch(ArrayIndexOutOfBoundsException e){
-	    e.printStackTrace(System.out);}
+		
 	return false;
     }
-     private boolean removeQueen(int r, int c){
-	try{
+    private boolean removeQueen(int r, int c){
 	int row=r;
 	int col=c;
-	    if (board[r][c]==-1){
-		board[r][c]=0;
-		while (row> 0 && col < board.length-1){
-		    board[row-1][col+1]-=1;
-		    row--;
-		    col++;
-		}
-		row=r;
-		col=c+1;
-	        while (col <board[0].length){
-		    board[r][col]-=1;
-		    col++;
-		}
-		row=r;
-		col=c;
-		while (row< board.length-1 && col < board[0].length-1){
-		    board[row+1][col+1]-=1;
-		    row++;
-		    col++;
-		}
-				
+	if (board[r][c]==-1){
+	    board[r][c]=0;
+	    while (row> 0 && col < board.length-1){
+		board[row-1][col+1]-=1;
+		row--;
+		col++;
 	    }
-		return true;
+	    row=r;
+	    col=c+1;
+	    while (col <board[0].length){
+		board[r][col]-=1;
+		col++;
+	    }
+	    row=r;
+	    col=c;
+	    while (row< board.length-1 && col < board[0].length-1){
+		board[row+1][col+1]-=1;
+		row++;
+		col++;
+	    }
+	    return true;		
 	}
-	catch(ArrayIndexOutOfBoundsException e){
-	    e.printStackTrace(System.out);}
+		
 	return false;
     }
     public String toString(){
 	String gatherer="";
 	for(int r=0;r<board.length;r++){
 	    for(int c=0;c<board.length;c++){
-	        gatherer= gatherer +board[r][c] + " ";
+	        gatherer= gatherer + board[r][c] + " ";
 	    }
 	    gatherer = gatherer + "\n";
 	}
 	return gatherer;
     }
-    public boolean solve(int col){
+    public boolean solveH(int col){
 	if (col>=board[0].length){
 	    return true;
 	}
 	for (int r=0;r<board.length;r++){
 	    if(addQueen(r,col)){
-		if (solve(col+1)){
+		if (solveH(col+1)){
 		    return true;
 		}
-		removeQueen(r,col);
 	    }
+	    removeQueen(r,col);
 	}
 	return false;
     }
+    public boolean solve(){
+	for (int r=0;r<board.length;r++){
+	    for (int c=0;c<board.length;c++){
+		if (board[r][c]!=0){
+		    throw new IllegalStateException("Your board cannot start with any nonzero numbers");
+		}
+	    }
+	}
+	return solveH(0);
+    }
+	
+    public int countSolutionsH(int col){
+	int soln = 0;
+	if (col>=board[0].length){
+	    return 1;
+	}           
+	for (int r=0;r<board.length;r++){
+	    if(addQueen(r,col)){
+		soln+=countSolutionsH(col+1);
+		removeQueen(r,col);
+	    }
+	}
+	return soln;
+    }
 
-    //public int countSolution(){}
+	
+
+
+    public int countSolutions(){
+	for (int r=0;r<board.length;r++){
+	    for (int c=0;c<board.length;c++){
+		if (board[r][c]!=0){
+		    throw new IllegalStateException("Your board cannot start with any nonzero numbers");
+		}
+	    }
+	}
+	return countSolutionsH(0);
+    }
 
     public static void main(String[] args){
-        QueenBoard test = new QueenBoard(2);
-	//test.addQueen(0,0);
+        QueenBoard test = new QueenBoard(10);
+	//	test.addQueen(0,0);
 	//System.out.println(test);
-	/*test.addQueen(2,1);
-	System.out.println(test);
-	test.removeQueen(2,1);
-	System.out.println(test);*/
-	System.out.println(test);
+	//System.out.println(test.solveH(0));
+	System.out.println(test.countSolutions());
+	//test.removeQueen(2,1);
+	//System.out.println(test);
+	//System.out.println(test.solve(0));
+	//System.out.println(test);
     }
 }
