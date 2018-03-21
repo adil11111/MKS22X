2208a231049e93ff22b3299f26b26922d430126a
Adil Gondal <agondal@stuy.edu>
2018-03-21 16:59:04 -0400

    wrote quickSort

--- 06QuickSort/Quick.java
+++ 06QuickSort/Quick.java
@@ -1,8 +1,8 @@
 import java.util.*;
 import java.io.*;
 public class Quick{
-
-    /*public static int partition ( int [] data, int start, int end){
+    /*
+    public static int partition ( int [] data, int start, int end){
 	Random gen = new Random();
 	int num= start + gen.nextInt(end - start + 1);
 	swap(data,start,num);
@@ -19,7 +19,8 @@ public class Quick{
 	}
 	swap(data,start,large);
 	return large;	
-	}*/
+	}
+    */
     public static int partition ( int [] data, int start, int end){
 	Random gen = new Random();
 	int num= start + gen.nextInt(end - start + 1);
@@ -27,7 +28,7 @@ public class Quick{
 	int small=start+1;
 	int large=end;
 	int index=small;
-	while (small <= large){
+	while (index <= large){
 	    if(data[index]==data[start]){
 		index++;
 	    }
@@ -43,7 +44,7 @@ public class Quick{
 	}
 	swap(data,start,large);
 	return large;	
-    }
+	}
 
     public static void swap(int[] data, int first, int second){
 	int temp = data[second];
@@ -74,15 +75,27 @@ public class Quick{
 	}
 	return ary[i];
     }
+    public static void quickSort(int[] ary){
+	quickH(ary,0,ary.length-1);
+    }
+    public static void quickH(int[] ary,int small,int large){
+	if(small<large){   
+	    int half = partition(ary, small, large);
+	    quickH(ary, small, half - 1);
+	    quickH(ary, half + 1, large);
+	}
+    }
     public static void main(String[] args){
-	int[] data= { 2, 10, 15, 23, 0,  5};
-        //System.out.println(partition(data,0,7));
-	System.out.println(quickSelect(data,0));
-		System.out.println(quickSelect(data,1));
-	System.out.println(quickSelect(data,2));
-	System.out.println(quickSelect(data,3));
-	System.out.println(quickSelect(data,4));
-	System.out.println(quickSelect(data,5));
-	    
+	int[] data= {0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2,0,1,1,2,2,2,3,4,2,3,4,5,2,1,3,4,2,1,3,2,3,4,1,2,3,2,3,2,1,1,3,2,4,5,6,2,1,3};
+	Random randgen= new Random();
+                //System.out.println(partition(data,0,7));
+	//System.out.println(quickSelect(data,0));
+	quickSort(data);
+	for(int i=0;i<data.length;i++){
+	    System.out.println(data[i]);
+	}
+	/*for (int i=0;i<data.length;i++){
+	    System.out.println(data[i]);
+	    }*/
     }
 }
