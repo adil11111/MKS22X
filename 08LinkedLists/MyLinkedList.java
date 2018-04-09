4d38489ad603ef96cf951eb4e4bd91ab845fa264
Adil Gondal <agondal@stuy.edu>
2018-04-09 14:50:42 -0400

    added toString

--- 08LinkedLists/MyLinkedList.java
+++ 08LinkedLists/MyLinkedList.java
@@ -1,4 +1,39 @@
 public class MyLinkedList{
+    private class Node{
+
+    private int data;
+    private Node next;
+    private Node previous;
+    
+    public Node(int value){
+	data=value;
+	next=null;
+	previous=null;
+    }
+    public Node getnext(){
+	return next;
+    }
+    public Node getPrev(){
+	return previous;
+    }
+    public int getValue(){
+	return data;
+    }
+    public void setnext(Node newNode){
+	next=newNode;
+    }
+    public void setPrev(Node newNode){
+	previous=newNode;
+    }
+    public void setValue(int newValue){
+        data= newValue;
+    }
+	public String toString(Node n){
+	    String result= ""+ n.getValue();
+	    return result;
+	}
+	    
+}
     private Node start;
     private Node end;
     private int size;
