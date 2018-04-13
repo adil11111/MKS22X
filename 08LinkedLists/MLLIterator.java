7c5e2790261ad53ef2cbbc8388c1ee6dfcfbf104
Adil Gondal <agondal@stuy.edu>
2018-04-13 14:48:34 -0400

    iterable class

---
+++ 08LinkedLists/MLLIterator.java
@@ -0,0 +1,12 @@
+import java.util.Iterator;
+public class MLLIterator.java{
+    public MLLIterator(){
+        
+    }
+    public void remove(){
+	throw new UnsupportedOperationException();
+    }
+    public String next(){
+	
+    }
+}
