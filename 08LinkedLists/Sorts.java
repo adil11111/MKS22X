49cbb5fa1aaa863c2d09d4348006bbeb401e3da6
Adil Gondal <agondal@stuy.edu>
2018-04-17 20:56:51 -0400

    trying to fix sort with negatives

--- 08LinkedLists/Sorts.java
+++ 08LinkedLists/Sorts.java
@@ -27,6 +27,9 @@ public class Sorts{
 	    index++;
 	}
     }
+    public static void radixsortIncludingNegatives(MyLinkedListImproved<Integer> data){ 
+   
+    }
     public static void radixsortNeg(MyLinkedListImproved<Integer> data){
 	int  maxIndex=data.max();
 	int maxNum=data.get(maxIndex);
@@ -40,15 +43,19 @@ public class Sorts{
 	while(index < maxLength){
 	    for(int i=0;i<data.size();i++){
 		int num = data.get(i);
+		System.out.println(num);
 		int newNum = Math.abs(num);
+		System.out.println(newNum);
 		if(index!=0){
 		    newNum=num/((int)Math.pow(10,index));
 		}
-		int digit = newNum % 10;
+		int digit = Math.abs(newNum % 10);
+		System.out.println(digit);
 		bucket[digit].add(num);
+		System.out.println(data);
 	    }
 	    data.clear();
-	    int negI=bucket.length;
+	    int negI=bucket.length-1;
 	    while(negI>0){
 		data.extend(bucket[negI]);
 		negI--;
@@ -58,6 +65,14 @@ public class Sorts{
     }
 
     public static void main(String[] args) {
+	MyLinkedListImproved<Integer> data = new MyLinkedListImproved<>();
+	data.add(-8);
+	data.add(-90);
+	data.add(-2);
+	data.add(-6);
+	//data.add(-19);
+	radixsortNeg(data);
+	System.out.println(data);
     }
     
 }
