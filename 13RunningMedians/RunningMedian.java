b5f6cac51341aaa85c5daac322adace42e6d2525
Adil Gondal <agondal@stuy.edu>
2018-05-14 22:04:32 -0400

    finished RunningMedian

--- 13RunningMedians/RunningMedian.java
+++ 13RunningMedians/RunningMedian.java
@@ -2,6 +2,8 @@ public class RunningMedian{
     private MyHeap<Double> min;
     private MyHeap<Double> max;
     private int size;
+    private Double median;
+
     
     public RunningMedian(){
 	max = new MyHeap<Double>();
@@ -11,12 +13,41 @@ public class RunningMedian{
     //- makes an empty container for Doubles.
 
     public void add(Double num){
+	if(size==0){
+	    max.add(num);
+	    median=num;
+	}
+	else{
+	    if (num>median){
+		min.add(num);
+	    }
+	    else{
+		max.add(num);
+	    }
+	}
+	if((max.size() - min.size()) >=2){
+	    Double result = max.remove();
+	    min.add(result);
+	}
+	else if((min.size() - max.size()) >= 2){
+	    Double result = min.remove();
+	    max.add(result);
+	}
 	size++;
     }
     //- insert a Double into the data structure.
+    public double getMedian(){
+	if (min.size()>max.size()){
+	    median = min.peek();
+	}
 
-    public Double getMedian(){
-	return 0.0;
+	else if(max.size()> min.size()){
+	    median = max.peek();
+	}
+	else{
+	    median = (min.peek()+max.peek()) / 2;
+	}
+	return median;	
     }
     //- return the current median, throws NoSuchElementException when size is 0.
 
