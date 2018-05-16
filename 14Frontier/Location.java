766212d5456b23ec3e19e7afeab5093214c01995
Adil Gondal <agondal@stuy.edu>
2018-05-15 22:56:56 -0400

    location file

---
+++ 14Frontier/Location.java
@@ -0,0 +1,40 @@
+public class Location implements Comparable<Location>{
+    private int x,y;
+    private Location previous;
+    private int distance;
+    private int distanceSoFar;
+
+    public Location(int _x, int _y, Location prev){
+	x = _x;
+	y = _y;
+	previous = prev;
+    }
+    public Location(int _x, int _y, Location prev,int dist,int dSF){
+	x=_x;
+	y=_y;
+	previous=prev;
+	distance=dist;
+	distanceSoFar=dSF;
+    }
+    public Location getPrevious(){
+	return previous;
+    }
+    public int getDistanceSoFar(){
+	return distanceSoFar;
+    }
+
+    public int getDistance(){
+	return distance;
+    }
+    public int getX(){
+	return x;
+    }
+    
+    public int getY(){
+	return y;
+    }
+    public int compareTo(Location loc){
+	return getDistance() - loc.getDistance();
+    }
+
+}
